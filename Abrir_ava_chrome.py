c3c47fb71bb1821155bc1b4ef9481c17d0442e6b
YarA <104739868+y93r@users.noreply.github.com>
2023-02-15 17:32:18 -0300

    Create Abrir_ava_chrome.py

---
+++ Abrir_ava_chrome.py
@@ -0,0 +1,23 @@
+#Google Chrome
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+#abrir o navegador e atualizar o chrome driver caso necessário
+navegador = webdriver.Chrome(ChromeDriverManager().install())
+navegador.get('https://univirtus.uninter.com/ava/web/')
+navegador.maximize_window()
+
+#tempo para abrir a página
+time.sleep(1.5)
+
+#colocando login
+navegador.find_element(By.ID, 'ru').send_keys(*******************)
+navegador.find_element(By.ID, 'senha').send_keys("***************")
+navegador.find_element(By.ID, 'loginBtn').click()
+
+#clicando no univirtus
+time.sleep(1.0)
+navegador.find_element(By.ID, 'loginBoxAva').click()
